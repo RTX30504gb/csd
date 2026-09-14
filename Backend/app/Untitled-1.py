7a1635a2be2ac1592275de1143a2102dfc290b8f
unknown <hkiscool4@gmail.com>
2026-09-14 17:19:54 +0200

    final

---
+++ Backend/app/Untitled-1.py
@@ -0,0 +1,3 @@
+print ("sonish is a gay femboy undeserving of love and respect")
+print ("payal is a lowlife pajeet who is a disgrace to humanity")
+print ("akshat is a faggot who is a disgrace to humanity")
